b71bca0021ba2d5690181b313fe7ae8c53c0de0b
igor <igor@igor.com>
2024-03-15 14:32:05 +0100

    third commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let hejka = "lul"
+  const [usersList, setUsersList] = useState([])
+
+  async function fetchData(){
+    console.log("Im fething")
+    try{
+      const res = await fetch("http://localhost:8080/api/users", {method: "GET"})
+      if(!res.ok){
+        throw new Error(`network response was not ok: ${res.status}`)
+      }
+      const data = await res.json()
+      setUsersList(data)
+    }catch(err){
+      console.log("Error:", err)
+    }
+  }
+
   return (
     <>
-    <h1>{hejka}</h1>
+    <h1>List of Users</h1>
+    <h2>Users:</h2>
+    <button onClick={fetchData}>Pobierz dane</button>
+    <ul>
+      {usersList.map((user)=>{
+      return (<li key={user._id}>
+        name: {user.name}; email: {user.email}; wiek: {user.age}
+      </li>)
+      })}
+    </ul>
     </>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const mongoose = require("mongoose")
-
+const cors = require("cors")
 
 const app = express()
 const PORT = 8080
 
 app.use(express.json())
+app.use(cors())
 
 const myDataBase = "myDB"
 const url = `mongodb://localhost:27017/${myDataBase}`
